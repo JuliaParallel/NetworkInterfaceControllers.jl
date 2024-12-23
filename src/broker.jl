5ea5b6d2f6ff3be8c48eb81ad5e92eb9de3eec3a
Johannes Blaschke <johannes@blaschke.science>
2024-12-23 08:38:33 -0800

    wip on broker database

--- src/broker.jl
+++ src/broker.jl
@@ -19,35 +19,87 @@ end
 
 struct Database
     usage::Dict{String, Dict{String, UInt64}}
+    lock::ReentrantLock
 
     function Database()
-        new(Dict{String, Dict{String, UInt64}}())
+        new(
+            Dict{String, Dict{String, UInt64}}(),
+            ReentrantLock()
+        )
     end
 end
 
 
-function random_free_port(start=1000, stop=61000, max_attempts=100)
+function transaction(db::Database, ifd::InterfaceData)
+    selected_interface::String
+    lock(db.lock) do
+        # We'll be iterating over these interfaces only -- this way the ifd
+        # interface list can be used to select valid interfaces
+        hostname   = ifd.hostname
+        interfaces = ifd.interfaces
+
+        # If no preexisting entry => create one in the db
+        if !(hostname in keys(db.usage))
+            db.usage[hostname] = Dict{String, UInt64}()
+            for interface in ifd.interfaces
+                db.usage[hostname][interface] = 0
+            end
+        end
+
+        # We'll be working on this entry ...
+        host_entry = db.usage[hostname]
+
+        # Check the database entry if all the interfaces are recorded. Iterate
+        # over the ifd interfaces only; the search for the least-used interface
+        # only considers the ifd interface list
+        for interface in interfaces
+            if !(interface in keys(host_entry))
+                host_entry[interface] = 0
+            end
+        end
+
+        # Find the first interface that has the lowest count. Iterate over the
+        # ifd interface list only; this is a feature allowing the user to
+        # whitelist which interfaces to choose from
+        lowest_count = typemax(UInt64)
+        for interface in interfaces
+            if lowest_count > host_entry[interface]
+                lowest_count = host_entry[interface]
+            end
+        end
+        # Once we have the lowest interface usage count, we select whichever
+        # interface has that lowest count and we come accross first -- yay!
+        for interface in interfaces
+            if lowest_count == host_entry[interface]
+                host_entry[interface] += 1
+                selected_interface = interface
+            end
+        end
+    end
+    return InterfaceAssignment(selected_interface)
+end
+
+
+function random_free_port(ip::IPv4; start=1000, stop=61000, max_attempts=100)
     for _ in 1:max_attempts
-        port::Int32 = rand(start:stop)
+        port::UInt32 = rand(start:stop)
         try
-            # TODO: make inter address configurable
-            server::Sockets.TCPServer = listen(port)
+            server::Sockets.TCPServer = listen(ip, port)
             close(server)
 
-            @debug "Found free port at: $(Int32(port))"
+            @debug "Found free port $(ip):$(Int32(port))"
             return TxnPort(port)
         catch e
-            @debug "Error $(e); Port $(Int32(port)) is busy, trying another..."
+            @debug "$(e); For $(ip):$(Int32(port)), trying another..."
 
         end
     end
-    error("Could not find a free port after $max_attempts attempts.")
+    @error "Could not find a free port after $(max_attempts) attempts."
 end
 
 
-function start_listener(port)
-    # TODO: make inter address configurable
-    server::TCPServer = listen(port)
+function start_listener(ip::IPv4, port::UInt32)
+    server::TCPServer = listen(ip, port)
 
     while true
         conn = accept(server)
@@ -56,14 +108,14 @@ function start_listener(port)
         @async begin
             try
                 while true
-                    txn_port::TxnPort = random_free_port()
+                    txn_port::TxnPort = random_free_port(ip)
                     line::IOBuffer    = IOBuffer()
                     serialize(line, txn_port)
                     seekstart(line)
                     write(conn, line)
                 end
             catch err
-                print("connection ended with error $err")
+                @error "Connection ended with error $(err)."
             end
         end
     end
