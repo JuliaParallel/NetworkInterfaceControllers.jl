5eaf816241af64b250c09fd794af2cea02a55dd2
Johannes Blaschke <jpblaschke@lbl.gov>
2025-01-13 10:44:47 -0800

    add more convenience functions

--- src/NetworkInterfaceControllers.jl
+++ src/NetworkInterfaceControllers.jl
@@ -23,13 +23,28 @@ get_hwloc_selector() = Base.get_extension(@__MODULE__, :HwlocSelector)
 include("hostlists.jl")
 using .Hostlists
 
-function start_broker(ipv)
+#------------------------------------------------------------------------------
+# Helper functions for Broker
+#------------------------------------------------------------------------------
+
+function julia_runtime_str()::String
+    julia_str   = Base.julia_cmd().exec |> first
+    project_str = Base.active_project()
+    return "$(julia_str) --project=$(project_str)"
+end
+
+function broker_ip_port(ipv)
     iface = get_interface_data(ipv, loopback=true) |>
             x->NameSelector.best_interfaces(
                 x,
                 NICPreferences.BROKER_INTERFACE.name,
                 NICPreferences.BROKER_INTERFACE.match_strategy) |>
             only
+    return iface.ip, NICPreferences.BROKER_INTERFACE.port
+end
+
+function start_broker(ipv)
+    ip, port = broker_port(ipv)
 
     t::Task = @task Broker.start_server(
         iface.ip, UInt32(NICPreferences.BROKER_INTERFACE.port)
@@ -39,27 +54,46 @@ function start_broker(ipv)
     return iface.ip, NICPreferences.BROKER_INTERFACE.port, t
 end
 
+function broker_ip_string(ipv::Int)::String
+    @assert ipv in (4, 6)
+
+    runtime_str = julia_runtime_str()
+    import_str  = "using NetworkInterfaceControllers, Sockets"
+    query_str   = (4==ipv) ? "broker_ip_port(IPv4)" : "broker_ip_port(IPv6)"
+
+    return "$(runtime_str) -e '$(import_str); println($(query_str) |> first)'"
+end
+
+function broker_port_string(ipv::Int)::String
+    @assert ipv in (4, 6)
+
+    runtime_str = julia_runtime_str()
+    import_str  = "using NetworkInterfaceControllers, Sockets"
+    query_str   = (4==ipv) ? "broker_ip_port(IPv4)" : "broker_ip_port(IPv6)"
+
+    return "$(runtime_str) -e '$(import_str); println($(query_str) |> last)'"
+end
+
 function broker_startup_string(ipv::Int)::String
     @assert ipv in (4, 6)
 
-    julia_str   = Base.julia_cmd().exec |> first
-    project_str = Base.active_project()
+    runtime_str = julia_runtime_str()
     import_str  = "using NetworkInterfaceControllers, Sockets"
-    query_str = (4==ipv) ? "ip,p,t=start_broker(IPv4)" : "ip,p,t=start_broker(IPv6)"
+    query_str   = (4==ipv) ? "start_broker(IPv4)" : "start_broker(IPv6)"
 
-    return "$(julia_str) --project=$(project_str) -e '$(import_str); $(query_str); println(\"\$(ip):\$(p)\"); wait(t)'"
+    return "$(runtime_str) -e '$(import_str); $(query_str) |> last |> wait'"
 end
 
 function broker_query_string(ip::String, port::Int)::String
-    julia_str   = Base.julia_cmd().exec |> first
-    project_str = Base.active_project()
+    runtime_str = julia_runtime_str()
     import_str  = "using NetworkInterfaceControllers.Broker, Sockets"
     query_str   = "Broker.query(ip\"$(ip)\", UInt32($(port)), ifaces)"
 
-    return "$(julia_str) --project=$(project_str) -e '$(import_str); $(query_str)'"
+    return "$(runtime_str) -e '$(import_str); $(query_str)'"
 end
 
-export start_broker, broker_startup_string, broker_query_string
+export start_broker, broker_ip_port, broker_ip_string, broker_port_string
+export broker_startup_string, broker_query_string
 
 function best_interface_hwloc_closest(
         data::Interface; pid::Union{T, Nothing}=nothing
