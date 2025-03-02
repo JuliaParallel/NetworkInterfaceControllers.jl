5a38d082fdec4e6cdc546078f48fbbaafc92f58a
Johannes Blaschke <johannes@blaschke.science>
2025-03-01 21:05:04 -0800

    use blacklist, not whitelist

--- test/configure_linux.jl
+++ test/configure_linux.jl
@@ -3,7 +3,7 @@ using NetworkInterfaceControllers.NICPreferences
 NICPreferences.configure!(
     name_selector_mode=setting(:mode, USE_ALWAYS),
     preferred_interface=setting(:interface, "^wlp[0-9]*s[0-9]*\$", MATCH_REGEX),
-    interface_name_whitelist=["docker0"],
+    interface_name_blacklist=["docker0"],
     hwloc_selector_mode=setting(:mode, USE_ALWAYS),
     hwloc_nic_pci_class="Network",
     broker_mode=setting(:mode, USE_ALWAYS),
