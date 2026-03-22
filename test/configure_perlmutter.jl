8d1bd1613b7f6860b672b0722a5a88fea8afbd54
Johannes Blaschke <jpblaschke@lbl.gov>
2026-03-22 15:13:57 -0700

    using public hsn for broker

--- test/configure_perlmutter.jl
+++ test/configure_perlmutter.jl
@@ -5,7 +5,7 @@ NICPreferences.configure!(
     preferred_interface=setting(:interface, "^hsn[0-9]*\$", MATCH_REGEX),
     interface_name_blacklist=[
         "nmn0", "nmn1", "net0", "net1", " net2", "net3", "net4", "net5",
-        "mlx5_bond_0"
+        "mlx5_bond_0", "bond0", "nmnb0", "nmnn1"
     ],
     hwloc_selector_mode=setting(
         :mode, USE_HOSTNAME,
@@ -16,6 +16,6 @@ NICPreferences.configure!(
         :mode, USE_HOSTNAME,
         "login[01-40],nid[200000-200255,004072-007200]"
     ),
-    broker_interface=setting(:interface, "hsn0", MATCH_EXACT, 3000),
+    broker_interface=setting(:interface, "hsnb0:chn", MATCH_EXACT, 3000),
     broker_host_env=["JULIA_NIC_HOST"]
 )
